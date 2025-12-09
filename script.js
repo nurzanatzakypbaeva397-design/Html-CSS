e7aa87d0090764e1a0aa66674f117a620f9f803c
nur <nurzanatzakypbaeva397@gmail.com>
2025-12-09 19:47:30 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,79 @@
+class Calculator {
+    constructor(previousOperandElement, currentOperandElement) {
+        this.previousOperandElement = previousOperandElement;
+        this.currentOperandElement = currentOperandElement;
+        this.clear();
+    }
+
+    clear() {
+        this.currentOperand = '';
+        this.previousOperand = '';
+        this.operation = undefined;
+        this.updateDisplay();
+    }
+
+    delete() {
+        this.currentOperand = this.currentOperand.toString().slice(0, -1);
+        this.updateDisplay();
+    }
+
+    chooseNumber(number) {
+        if (number === '.' && this.currentOperand.includes('.')) return;
+        this.currentOperand = this.currentOperand.toString() + number.toString();
+        this.updateDisplay();
+    }
+
+    chooseOperation(operation) {
+        if (this.currentOperand === '') return;
+        if (this.previousOperand !== '') {
+            this.compute();
+        }
+        this.operation = operation;
+        this.previousOperand = this.currentOperand;
+        this.currentOperand = '';
+        this.updateDisplay();
+    }
+
+    compute() {
+        let computation;
+        const prev = parseFloat(this.previousOperand);
+        const current = parseFloat(this.currentOperand);
+        if (isNaN(prev) || isNaN(current)) return;
+
+        switch (this.operation) {
+            case '+':
+                computation = prev + current;
+                break;
+            case '-':
+                computation = prev - current;
+                break;
+            case '×':
+                computation = prev * current;
+                break;
+            case '÷':
+                computation = prev / current;
+                break;
+            default:
+                return;
+        }
+        this.currentOperand = computation;
+        this.operation = undefined;
+        this.previousOperand = '';
+        this.updateDisplay();
+    }
+
+    updateDisplay() {
+        this.currentOperandElement.innerText = this.currentOperand || '0';
+        if (this.operation != null) {
+            this.previousOperandElement.innerText = 
+                `${this.previousOperand} ${this.operation}`;
+        } else {
+            this.previousOperandElement.innerText = '';
+        }
+    }
+}
+
+const previousOperandElement = document.getElementById('previousOperand');
+const currentOperandElement = document.getElementById('currentOperand');
+
+const calculator = new Calculator(previousOperandElement, currentOperandElement);
